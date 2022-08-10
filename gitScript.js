2ec1274deaa5bced8f293a8ca372b7d1b31ffaf7
Kacper Zacharz <xxygrek@wp.pl>
2022-08-10 09:05:52 +0200

    stash

--- gitScript.js
+++ gitScript.js
@@ -5,8 +5,12 @@
 "hi";
 "hi ben";
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 "text";
 >>>>>>> Stashed changes
 =======
 "89";
+>>>>>>> Stashed changes
+=======
+"minnor changes";
 >>>>>>> Stashed changes
